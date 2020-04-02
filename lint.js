31ba3861c85166fd1eb14fd83410a4dbb7f98903
Guy <guyden1997@gmail.com>
2020-04-02 23:13:58 +1100

    right click menu

--- lint.js
+++ lint.js
@@ -5,17 +5,23 @@ window.addEventListener('load', () => {
     let overlay = document.getElementById("error-overlay");
     let feedbackBox = document.getElementById("feedback");
     let matches =[];
+    let scanedDivs = true;
     //detect when user change text WritingPad
     //rerun models
     resizeWritingPad();
     pad.addEventListener("input", () => {
+        scanPad();
+    }, false);
+
+    function scanPad(){
         resizeWritingPad();
         overlay.innerHTML= "";
+        scanedDivs = true;
         let lineNumber = 0;
         matches = []
         for (let sentence of pad.value.split("\n")) {
             let rule = void 0; 
-            sentence = sentence.replace(/</g, "&lt"); // XXS Prevention
+            sentence = sentence.replace(/<\/?span[^>]*>/g); // XXS Prevention
             let sentenceWithErrors = sentence;
             let interimMatches = [];
             let interimMatch = "";
@@ -35,7 +41,7 @@ window.addEventListener('load', () => {
                 return -1
             })
 
-            //Reduce overlapping divs
+            //Reduce overlapping errors
             let temp =[]
             for(let i = 0; i < interimMatches.length; i++ ){
                 //div overlapping
@@ -43,14 +49,8 @@ window.addEventListener('load', () => {
                     temp[temp.length-1].position[1] = interimMatches[i].position[1];
                 }
                 else{
-                    if(interimMatch != null){
-
-                        //generate random id 
-                        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-                        for ( let i = 0; i < 7; i++ ) {
-                            result += characters.charAt(Math.floor(Math.random() * characters.length));
-                        }
-                        interimMatch[i].id =  result;
+                    if(interimMatches[i] != null){
+                        interimMatches[i].id =  randomID();
                     }
                     temp.push(interimMatches[i]);
                 }
@@ -69,37 +69,9 @@ window.addEventListener('load', () => {
             lineNumber++;
 
             //generate context menu for error
-            for(let i in interimMatches){
-                document.getElementById(i.id).addEventListener("contextmenu", (e) => {
-                    removeContextMenu();
-                    e.preventDefault()
-                    console.log("right click listeners");
-                    let contextmenu = document.createElement("div");
-                    contextmenu.id ="rightClickMenu";
-
-                    //set posistion of context menu 
-                    console.log(e.pageX+ " "+ e.pageY);
-                    contextmenu.style.left =  e.pageX + "px";
-                    contextmenu.style.top = e.pageY + "px";
-
-                    // add values to context menu
-                    let options = i.rule.options
-                    for(let i of options ){
-                        let action = document.createElement("div");
-                        console.log(i);
-                        action.innerText = i;
-                        action.classList.add("actionItem");
-                        contextmenu.appendChild(action);
-                    }
-                    document.body.appendChild(contextmenu);
-                    return false;
-                
-                })
-            }
             matches.push(interimMatches);
         }
-    }, false);
-
+    }
 
     pad.addEventListener("click", () => {
         let linesLength = [];
@@ -127,14 +99,89 @@ window.addEventListener('load', () => {
         pad.style.height = pad.scrollHeight +"px";
         pad.style.width = pad.scrollWidth +"px";
     }
-    
+
+    document.addEventListener("contextmenu", (e) =>{ 
+
+        // bring overlay to top layer
+        overlay.style.zIndex = 30
+        let element = document.elementFromPoint(e.pageX, e.pageY);
+        overlay.style.zIndex = 0;
+
+        removeContextMenu();
+        e.preventDefault()
+        if(element != null){
+        for(let j of matches){
+            for(let i of j ){
+            if(i.id == element.id){
+                console.log("right click deploy");
+                let contextmenu = document.createElement("div");
+                contextmenu.id ="rightClickMenu";
+                //set posistion of context menu 
+                console.log(e.pageX+ " " + e.pageY);
+                contextmenu.style.left =  e.pageX + "px";
+                contextmenu.style.top = e.pageY + "px";
+                // add values to context menu
+                let options = i.rule.options
+                if(options != null){
+                    for(let z of options ){
+                        let action = document.createElement("div");
+                        console.log(z);
+                        if(z != ""){
+                            action.innerText = z;
+                        }
+                        else{
+                            action.innerText = "delete";
+                        }
+                        action.classList.add("actionItem");
+                        action.addEventListener("click", () => {       
+                            let text = pad.value.substr(0, getAbsolutePosision(i.lineNumber,i.position[0])) + 
+                            z +
+                            pad.value.substr(getAbsolutePosision(i.lineNumber,i.position[1]), pad.value.length);
+                            pad.value = text;
+                            scanPad();
+                        });
+                        contextmenu.appendChild(action);
+                    }
+                }
+                else{
+                    let action = document.createElement("div");
+                    action.innerText = "No action Avabile";
+                    action.classList.add("actionItem");
+                    contextmenu.appendChild(action);
+                }
+                document.body.appendChild(contextmenu);
+                pad.focus
+        }
+        }
+    }}
+    });
+    function getAbsolutePosision(lineNumber, linePosistion){
+        let linesLength = [];
+        pad.value.split("\n").map((line) => linesLength.push(line.length));
+
+        let lp =0;
+        for(let i=0; i<lineNumber; i++ ){
+            lp = lp + linesLength[i]+1;
+        }
+        console.log(lp + linePosistion)
+        return lp + linePosistion;
+    }
 });
+
 
 document.addEventListener( "click", (e) => {
     removeContextMenu();
 });
 
-
+function randomID(){
+    //generate random id 
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let randomID =""
+    for ( let i = 0; i < 7; i++ ) {
+        randomID += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return randomID;
+}
 function removeContextMenu(){
     let contextmenu = document.getElementById("rightClickMenu")
     if(contextmenu != null) contextmenu.remove()
--- scripts/rules_eng.js
+++ scripts/rules_eng.js
@@ -3,6 +3,9 @@
 
 // pattern is regex to match the common error
 //scope is the what the what lines the rule works on for example
+
+let defultText = "this will not replace close secutny of the text."
+
 export let ruleSet = 
     [{ "pattern": /\s;\s?/g, "options": ["; "], "type": "error" },
     { "pattern": /\s:\s?/g, "options": [": "] , "type": "error" },
@@ -19,11 +22,11 @@ export let ruleSet =
     { "pattern": /\.\s\.\s\./g, "options": ["...", ". ..."], "type": "error"  },
     { "pattern": /\.\.\.\s[\.|\?|\!]/g , "options": ["..."], "type": "error" },
     { "pattern": /\d{1,}(nd|rd|th|st)/g, "options":[], "usageNote": "the part of place should be superscript eg.2<sup>nd</sup><br><br> eg 2^{nd}", "type": "error"  },
-    { "pattern": /\s"\s/g, "options":[] , "type": "error"  },
+    { "pattern": /(\s|^)["|'](\s|$)/g, "options":[] , "type": "error"  },
     { "pattern": /[1-9]\s[1-9]{1,}\/[1-9]{1,}/g, "options":[] , "type": "error" },
     { "pattern": /\s[\!|\?]/g, "options":[] , "type": "error" },
     { "pattern": /~/g, "options":[] , "type": "error" },
-    { "pattern": /\d[=|*]\d/gi, "options":[] , "type": "error" },
+    { "pattern": /\d[=|\*]\d/gi, "options":[] , "type": "error" },
     { "pattern": /æ/g, "options":["[ae]"] , "type": "warning"},
     { "pattern": /œ/g, "options":["[oe]"] , "type": "warning"},
     { "pattern": /\_/g, "options":["-"] , "type": "special"},
@@ -37,12 +40,11 @@ export let ruleSet =
     { "pattern": /˜/g, "options":[] , "type": "error"},
     { "pattern": /¸/g, "options":[] , "type": "error"},
     { "pattern": /\^/g, "options":[] , "type": "special"},
-    { "pattern": /(?:(\w|\s))\*(?!\])(?:(\w|\s))/g, "options":["[*]"] , "type": "warning" },
+    { "pattern": /(?:($|\w|\s))\*(?!\])(?:(\w|\s|^))/g, "options":["[*]"] , "type": "warning" },
     { "pattern": /ſ/g, "options":["s", "S"] , "type": "error"},
     { "pattern": /[α-ωΑ-Ω]/g, "options":[] , "type": "error", "usageNote": "greek lettering see guide on wiki"},
     { "pattern": /\—/g, "options":["-","--"] , "type": "error"},
     { "pattern": /[†|‡|§]/g, "options":[] , "type": "error", "usageNote": "please replace alterntive with aphophy *"},
-    //{ "pattern": /(?<![\s|.])\.{2,3}(?![\.|\!|\?|\,])/g, "options":[" ... "] , "type": "error" },
     { "pattern": /(?:[a-z])[A-Z]{1,}/g, "options":[] , "type": "error" },
     {"pattern": /[\“|\”]/g , "options":["\""] , "type": "error", "usageNote": "please replace with \"" },
     { "pattern": /(?:\w)\"(?:\w)/g, "options":[] , "type": "error" },
@@ -55,7 +57,23 @@ export let ruleSet =
     {"pattern": /^-[\w]/g, "options":[] , "type": "error"},
     {"pattern": /\w*\s('nt|'s|'re|n't|'is)/g , "options":[] , "type": "error" },
     {"pattern": /-{1,}/g , "options":[] , "type": "warning" },
-
+    {"pattern": /\w\(/g , "options":[] , "type": "warning" },
+    {"pattern": /\)\w/g , "options":[] , "type": "warning" },
+    {"pattern": /^[0-9]{1,}$/g , "options":[] , "type": "warning", "usageNote":"please make sure you've removed all page numbers" },
+    {"pattern": /(?:[\w|;])\.{3}(?!\.)}/g , "options":[] , "type": "error" },
+    {"pattern": /\.{3,4}(?:\w)/g , "options":[] , "type": "error" },
+    {"pattern": /[\!|\.|\?]\s\.{3}/g , "options":[] , "type": "error" },
+    {"pattern": /\"\'/g , "options":[] , "type": "warning" },
+    {"pattern": /\/'/g , "options":[] , "type": "warning" },
+    {"pattern": /''/g , "options":[] , "type": "warning" },
+    {"pattern": /\b1\b/g , "options":[] , "type": "warning" },
+    {"pattern":/(?:\s)[b-hj-z]\b/gi, "options":[] , "type": "warning"},
+    {"pattern":/^[}|)||]]/gi, "options":[] , "type": "error"},
+    {"pattern":/^[,|.|]]/gi, "options":[] , "type": "error"},
+    {"pattern":/\w\.\w/gi, "options":[] , "type": "error"},
+    {"pattern":/[;|\.|\,|:]{2,}/gi, "options":[] , "type": "error"},
+    {"pattern":/¡/gi, "options":[] , "type": "error"},
+    {"pattern":/[0-9]S[0-9]/g, "options":[] , "type": "error"},
 ]
 // rules to add
 // rules to add space before end of line mark
@@ -74,11 +92,11 @@ export let ruleSet =
 // [U+0400–U+04FF] celyrlic
 // [Hebrew: **], or [Arabic: **].
 // ( ) { }
-// warnings
+// warnings 
 // ſ long s done
 // `
-// “ ”. 
-// would n't 't is did n't what 's  they 're does n't
+// “ ”. done
+// would n't 't is did n't what 's  they 're does n't /done
 // ---
 //?...
 //{ "pattern": /\*/g, "options":["[*]"] , "type": "warning" },
@@ -93,5 +111,7 @@ export let ruleSet =
 
 
 // a)prean 
-//They -they make the sky look
+//They -they make the sky look done
 // ' 
+//-'
+// ¡
