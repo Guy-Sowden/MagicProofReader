20bc240e279a975211e610606106be2efbc31fa5
Guy <guyden1997@gmail.com>
2020-04-16 22:17:34 +1000

    linting

--- lint.js
+++ lint.js
@@ -1,192 +1,209 @@
-import * as rules from "/scripts/rules_eng.js"
+import * as rules from '/scripts/rules_eng.js';
 
 window.addEventListener('load', () => {
-    let pad = document.getElementById("WritingPad");
-    let overlay = document.getElementById("error-overlay");
-    let feedbackBox = document.getElementById("feedback");
-    let matches =[];
-    let scanedDivs = true;
+  const pad = document.getElementById('WritingPad');
+  const overlay = document.getElementById('error-overlay');
+  const feedbackBox = document.getElementById('feedback');
+  let matches =[];
+  scanPad();
+  // detect when user change text WritingPad
+  // rerun models
+  resizeWritingPad();
+
+  pad.addEventListener('input', () => {
     scanPad();
-    //detect when user change text WritingPad
-    //rerun models
+  }, false);
+
+  /**
+ */
+  function scanPad() {
     resizeWritingPad();
-    pad.addEventListener("input", () => {
-        scanPad();
-    }, false);
+    overlay.innerHTML= '';
+    let lineNumber = 0;
+    matches = [];
+    for (let sentence of pad.value.split('\n')) {
+      sentence = sentence.replace(/</g, '&lt'); // XXS Prevention
+      let sentenceWithErrors = sentence;
+      let interimMatches = [];
+      let interimMatch;
+      for (const rule of rules.ruleSet) {
+        while ((interimMatch = rule.pattern.exec(sentence)) !== null) {
+          // add position of matches to array
+          interimMatches.push({
+            'position': [interimMatch.index +
+            interimMatch[0].indexOf(interimMatch[interimMatch.length -1]),
+            interimMatch.index +
+            interimMatch[0].indexOf(interimMatch[interimMatch.length -1]) +
+            interimMatch[interimMatch.length -1].length],
+            'rule': rule,
+            'lineNumber': lineNumber,
+          });
+        }
+      }
+      // sort matches by position
+      interimMatches = interimMatches.sort((a, b) =>{
+        if (a.position[0] > b.position[0]) return 1;
+        return -1;
+      });
 
-    function scanPad(){
-        resizeWritingPad();
-        overlay.innerHTML= "";
-        scanedDivs = true;
-        let lineNumber = 0;
-        matches = []
-        for (let sentence of pad.value.split("\n")) { 
-            sentence = sentence.replace(/</g, "&lt"); // XXS Prevention
-            let sentenceWithErrors = sentence;
-            let interimMatches = [];
-            let interimMatch;
-            for (let rule of rules.ruleSet){ 
-                while ((interimMatch = rule.pattern.exec(sentence)) !== null){
-                    console.log(interimMatch)
-                    // add position of matches to array
-                    interimMatches.push({
-                        "position":[interimMatch.index + 
-                            interimMatch[0].indexOf(interimMatch[interimMatch.length -1]), 
-                            interimMatch.index + interimMatch[0].indexOf(interimMatch[interimMatch.length -1]) + 
-                            interimMatch[interimMatch.length -1].length],
-                        "rule":rule,
-                        "lineNumber":lineNumber
-                    });
-                }
-            }
-            //sort matches by position 
-            interimMatches = interimMatches.sort((a , b) =>{
-                if(a.position[0] > b.position[0]) return 1 
-                return -1
-            })
+      // Reduce overlapping errors
+      const temp =[];
+      for (let i = 0; i < interimMatches.length; i++ ) {
+        // div overlapping
+        if (i > 0 && interimMatches[i-1].position[1] >= interimMatches[i]
+            .position[0]) {
+          temp[temp.length-1].position[1] = interimMatches[i].position[1];
+        } else {
+          if (interimMatches[i] != null) {
+            interimMatches[i].id = randomID();
+          }
+          temp.push(interimMatches[i]);
+        }
+      }
+      interimMatches = temp;
 
-            //Reduce overlapping errors
-            let temp =[]
-            for(let i = 0; i < interimMatches.length; i++ ){
-                //div overlapping
-                if(i > 0 && interimMatches[i-1].position[1] >= interimMatches[i].position[0]){
-                    temp[temp.length-1].position[1] = interimMatches[i].position[1];
-                }
-                else{
-                    if(interimMatches[i] != null){
-                        interimMatches[i].id =  randomID();
-                    }
-                    temp.push(interimMatches[i]);
-                }
-            }
-            interimMatches = temp
-
-            // Add underlines to errors and warnings
-            for (let i of interimMatches.reverse()){
-                sentenceWithErrors = sentenceWithErrors.substring(0, i.position[0]) +
-                "<span id=" + i.id + " class='text-"+ i.rule.type +"'>" + 
+      // Add underlines to errors and warnings
+      for (const i of interimMatches.reverse()) {
+        sentenceWithErrors = sentenceWithErrors.substring(0, i.position[0]) +
+                '<span id=' + i.id + ' class=\"text-'+ i.rule.type +'\">' +
                 sentenceWithErrors.substring(i.position[0], i.position[1])+
-                "</span>"+
-                sentenceWithErrors.substring(i.position[1],sentenceWithErrors.length);
-            }
-            overlay.innerHTML = overlay.innerHTML + sentenceWithErrors + "\n";
-            lineNumber++;
+                '</span>'+
+                sentenceWithErrors.substring(
+                    i.position[1], sentenceWithErrors.length);
+      }
+      overlay.innerHTML = overlay.innerHTML + sentenceWithErrors + '\n';
+      lineNumber++;
 
-            //generate context menu for error
-            matches.push(interimMatches);
-        }
+      matches.push(interimMatches);
     }
+  }
 
-    pad.addEventListener("click", () => {
-        let linesLength = [];
-        pad.value.split("\n").map((line) => linesLength.push(line.length));
+  pad.addEventListener('click', () => {
+    const linesLength = [];
+    pad.value.split('\n').map((line) => linesLength.push(line.length));
 
-        let cursorPosition = pad.selectionStart
+    const cursorPosition = pad.selectionStart;
 
-        let totalLineLength =0;
-        let lineIndex = 0;
-        for(let j of matches){
-            for(let i of j ){
-                // find length of predings line to convert absulute posistion in text box to line posistion
-                if(cursorPosition >= totalLineLength + i.position[0] && cursorPosition <= totalLineLength + i.position[1]){
-                    // set the text of the feedback box
-                    if(i.rule.usageNote != null) feedbackBox.innerHTML = i.rule.usageNote;
-                    else feedbackBox.innerText = ""
-                }
-            }
-            totalLineLength += linesLength[lineIndex] +1;
-            lineIndex++;
+    let totalLineLength =0;
+    let lineIndex = 0;
+    for (const j of matches) {
+      for (const i of j ) {
+        // find length of predings line to convert absulute posistion
+        // in text box to line posistion
+        if (cursorPosition >= totalLineLength + i.position[0] &&
+          cursorPosition <= totalLineLength + i.position[1]) {
+          // set the text of the feedback box
+          if (i.rule.usageNote != null) {
+            feedbackBox.innerHTML = i.rule.usageNote;
+          } else feedbackBox.innerText = '';
         }
-    });
-
-    function resizeWritingPad(){
-        pad.style.height = pad.scrollHeight +"px";
-        pad.style.width = pad.scrollWidth +"px";
+      }
+      totalLineLength += linesLength[lineIndex] +1;
+      lineIndex++;
     }
+  });
+  /**
+ */
+  function resizeWritingPad() {
+    pad.style.height = pad.scrollHeight +'px';
+    pad.style.width = pad.scrollWidth +'px';
+  }
 
-    document.addEventListener("contextmenu", (e) =>{ 
+  document.addEventListener('contextmenu', (e) =>{
+    // bring overlay to top layer
+    overlay.style.zIndex = 30;
+    const element = document.elementFromPoint(e.pageX, e.pageY);
+    overlay.style.zIndex = 0;
 
-        // bring overlay to top layer
-        overlay.style.zIndex = 30
-        let element = document.elementFromPoint(e.pageX, e.pageY);
-        overlay.style.zIndex = 0;
-
-        removeContextMenu();
-        e.preventDefault()
-        if(element != null){
-        for(let j of matches){
-            for(let i of j ){
-            if(i.id == element.id){
-                console.log("right click deploy");
-                let contextmenu = document.createElement("div");
-                contextmenu.id ="rightClickMenu";
-                //set posistion of context menu 
-                console.log(e.pageX+ " " + e.pageY);
-                contextmenu.style.left =  e.pageX + "px";
-                contextmenu.style.top = e.pageY + "px";
-                // add values to context menu
-                let options = i.rule.options
-                if(options != null){
-                    for(let z of options ){
-                        let action = document.createElement("div");
-                        console.log(z);
-                        if(z != ""){
-                            action.innerText = z;
-                        }
-                        else{
-                            action.innerText = "delete";
-                        }
-                        action.classList.add("actionItem");
-                        action.addEventListener("click", () => {       
-                            let text = pad.value.substr(0, getAbsolutePosision(i.lineNumber,i.position[0])) + 
-                            z +
-                            pad.value.substr(getAbsolutePosision(i.lineNumber,i.position[1]), pad.value.length);
-                            pad.value = text;
-                            scanPad();
-                        });
-                        contextmenu.appendChild(action);
-                    }
-                }
-                else{
-                    let action = document.createElement("div");
-                    action.innerText = "No action Avabile";
-                    action.classList.add("actionItem");
-                    contextmenu.appendChild(action);
-                }
-                document.body.appendChild(contextmenu);
-                pad.focus
-        }
-        }
-    }}
-    });
-    function getAbsolutePosision(lineNumber, linePosistion){
-        let linesLength = [];
-        pad.value.split("\n").map((line) => linesLength.push(line.length));
-
-        let lp =0;
-        for(let i=0; i<lineNumber; i++ ){
-            lp = lp + linesLength[i]+1;
-        }
-        console.log(lp + linePosistion)
-        return lp + linePosistion;
-    }
-});
-
-
-document.addEventListener( "click", (e) => {
     removeContextMenu();
+    if (element != null) {
+      for (const j of matches) {
+        for (const i of j ) {
+          if (i.id == element.id) {
+            const contextmenu = document.createElement('div');
+            contextmenu.id ='rightClickMenu';
+            // set posistion of context menu
+            contextmenu.style.left = e.pageX + 'px';
+            contextmenu.style.top = e.pageY + 'px';
+            // add values to context menu
+            const options = i.rule.options;
+            if (options != []) {
+              for (const z of options ) {
+                const action = document.createElement('div');
+                if (z != '') {
+                  action.innerText = z;
+                } else {
+                  action.innerText = 'delete';
+                }
+
+                action.classList.add('actionItem');
+                action.addEventListener('click', () => {
+                  const text = pad.value.substr(0,
+                      getAbsolutePosision(i.lineNumber, i.position[0])) +
+                  z +
+                  pad.value.substr(
+                      getAbsolutePosision(i.lineNumber, i.position[1]),
+                      pad.value.length,
+                  );
+                  pad.value = text;
+                  scanPad();
+                });
+                contextmenu.appendChild(action);
+              }
+            } else {
+              const action = document.createElement('div');
+              action.innerText = 'No action Avabile';
+              action.classList.add('actionItem');
+              contextmenu.appendChild(action);
+            }
+            e.preventDefault();
+            document.body.appendChild(contextmenu);
+            pad.focus;
+          }
+        }
+      }
+    }
+  });
+
+  /**
+ * @param {int} lineNumber
+ * @param {int} linePosistion
+ * @return {int}
+ */
+  function getAbsolutePosision(lineNumber, linePosistion) {
+    const linesLength = [];
+    pad.value.split('\n').map((line) => linesLength.push(line.length));
+
+    let lp =0;
+    for (let i=0; i<lineNumber; i++ ) {
+      lp = lp + linesLength[i] + 1;
+    }
+    return lp + linePosistion;
+  }
 });
 
-function randomID(){
-    //generate random id 
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    let randomID =""
-    for ( let i = 0; i < 7; i++ ) {
-        randomID += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return randomID;
+
+document.addEventListener( 'click', (e) => {
+  removeContextMenu();
+});
+
+/**
+ * @return {string}
+ */
+function randomID() {
+  // generate random id for error div
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let randomID ='';
+  for ( let i = 0; i < 7; i++ ) {
+    randomID += characters.charAt(Math.floor(Math.random() *
+    characters.length));
+  }
+  return randomID;
 }
-function removeContextMenu(){
-    let contextmenu = document.getElementById("rightClickMenu")
-    if(contextmenu != null) contextmenu.remove()
+
+/**
+ */
+function removeContextMenu() {
+  const contextmenu = document.getElementById('rightClickMenu');
+  if (contextmenu != null) contextmenu.remove();
 }
--- scripts/rules_eng.js
+++ scripts/rules_eng.js
@@ -2,116 +2,124 @@
 // that are meant to assure syntactical consistency
 
 // pattern is regex to match the common error
-//scope is the what the what lines the rule works on for example
+// scope is the what the what lines the rule works on for example
 
-let defultText = "this will not replace close secutny of the text."
+const defultText = 'this will not replace close secutny of the text.';
 
-export let ruleSet = 
-    [{ "pattern": /\s;\s?/g, "options": ["; "], "type": "error" },
-    { "pattern": /\s:\s?/g, "options": [": "] , "type": "error" },
-    { "pattern": /¾/g, "options": ["1/3"], "type": "error","usageNote": "wrong formatting of persentage ¾ sould be proofread as 3/4 " },
-    { "pattern": /¼/g, "options": ["1/4"], "type": "error", "usageNote": "wrong formatting of persentage ¼ sould be proofread as 1/4 " },
-    { "pattern": /½/g, "options": ["1/2"], "type": "error" },
-    { "pattern": /^\s/g, "options": [""], "type": "error", "usageNote": "Space at begining of line" },
-    { "pattern": /[0-9]{,4}o/, "options": ["°"], "type": "warning"  },
-    { "pattern": /(?:[a-z]|\s)\-$/g, "options": ["-", "--"], "usageNote":"bring other half of word on to this line", "type": "error"},
-    { "pattern": /\s{2,}/g, "options": [" "], "usageNote": "double space",  "type": "warning" },
-    { "pattern": /\s-{1,4}\s/g, "options": ["--"], "usageNote": "start a line with an em-dash. move the em-dash and first word of the sentance to previous line" },
-    { "pattern":  /\s?\(\s/g, "options": [" ("] , "type": "warning"  },
-    { "pattern":  /\s\)\s?/g, "options": [" )"],  "type": "warning"  },
-    { "pattern": /\.\s\.\s\./g, "options": ["...", ". ..."], "type": "error"  },
-    { "pattern": /\.\.\.\s[\.|\?|\!]/g , "options": ["..."], "type": "error" },
-    { "pattern": /\d{1,}(nd|rd|th|st)/g, "options":[], "usageNote": "the part of place should be superscript eg.2<sup>nd</sup><br><br> eg 2^{nd}", "type": "error"  },
-    { "pattern": /(\s|^)["|'](\s|$)/g, "options":[] , "type": "error"  },
-    { "pattern": /[1-9]\s[1-9]{1,}\/[1-9]{1,}/g, "options":[] , "type": "error" },
-    { "pattern": /\s[\!|\?]/g, "options":[] , "type": "error" },
-    { "pattern": /~/g, "options":[] , "type": "error" },
-    { "pattern": /\d[=|\*]\d/gi, "options":[] , "type": "error" },
-    { "pattern": /æ/g, "options":["[ae]"] , "type": "warning"},
-    { "pattern": /œ/g, "options":["[oe]"] , "type": "warning"},
-    { "pattern": /\_/g, "options":["-"] , "type": "special"},
-    { "pattern": /¯/g, "options":[] , "type": "error"},
-    { "pattern": /¨/g, "options":[] , "type": "error"},
-    { "pattern": /·/g, "options":[] , "type": "error"},
-    { "pattern": /`/g, "options":[] , "type": "error"},
-    { "pattern": /´/g, "options":[] , "type": "error"},
-    { "pattern": /∨/g, "options":[] , "type": "error"},
-    { "pattern": /∪/g, "options":[] , "type": "error"},
-    { "pattern": /˜/g, "options":[] , "type": "error"},
-    { "pattern": /¸/g, "options":[] , "type": "error"},
-    { "pattern": /\^/g, "options":[] , "type": "special"},
-    { "pattern": /(?:($|\w|\s))\*(?!\])(?:(\w|\s|^))/g, "options":["[*]"] , "type": "warning" },
-    { "pattern": /ſ/g, "options":["s", "S"] , "type": "error"},
-    { "pattern": /[α-ωΑ-Ω]/g, "options":[] , "type": "error", "usageNote": "greek lettering see guide on wiki"},
-    { "pattern": /\—/g, "options":["-","--"] , "type": "error"},
-    { "pattern": /[†|‡|§]/g, "options":[] , "type": "error", "usageNote": "please replace alterntive with aphophy *"},
-    { "pattern": /(?:[a-z])[A-Z]{1,}/g, "options":[] , "type": "error" },
-    {"pattern": /[\“|\”]/g , "options":["\""] , "type": "error", "usageNote": "please replace with \"" },
-    { "pattern": /(?:\w)\"(?:\w)/g, "options":[] , "type": "error" },
-    { "pattern": /\"\"/g, "options":[] , "type": "error" },
-    { "pattern": /[´|`]/g, "options":[] , "type": "error" },
-    { "pattern": /\w\s\-{1,}\s\w/g, "options":[] , "type": "error" },
-    {"pattern": /\w\s\-{1,}/g, "options":[] , "type": "error" },
-    {"pattern": /-{3}/g, "options":[] , "type": "error" },
-    {"pattern": /\s\!/g,"options":["!"] , "type": "error"},
-    {"pattern": /^-[\w]/g, "options":[] , "type": "error"},
-    {"pattern": /\w*\s('nt|'s|'re|n't|'is)/g , "options":[] , "type": "error" },
-    {"pattern": /-{1,}/g , "options":[] , "type": "warning" },
-    {"pattern": /\w\(/g , "options":[] , "type": "warning" },
-    {"pattern": /\)\w/g , "options":[] , "type": "warning" },
-    {"pattern": /^[0-9]{1,}$/g , "options":[] , "type": "warning", "usageNote":"please make sure you've removed all page numbers" },
-    {"pattern": /(?:[\w|;])\.{3}(?!\.)}/g , "options":[] , "type": "error" },
-    {"pattern": /\.{3,4}(?:\w)/g , "options":[] , "type": "error" },
-    {"pattern": /[\!|\.|\?]\s\.{3}/g , "options":[] , "type": "error" },
-    {"pattern": /\"\'/g , "options":[] , "type": "warning" },
-    {"pattern": /\/'/g , "options":[] , "type": "warning" },
-    {"pattern": /''/g , "options":[] , "type": "warning" },
-    {"pattern": /\b1\b/g , "options":[] , "type": "warning" },
-    {"pattern":/(?:\s)[b-hj-z]\b/gi, "options":[] , "type": "warning"},
-    {"pattern":/^[}|)||]]/gi, "options":[] , "type": "error"},
-    {"pattern":/^[,|.|]]/gi, "options":[] , "type": "error"},
-    {"pattern":/\w\.\w/gi, "options":[] , "type": "error"},
-    {"pattern":/[;|\.|\,|:]{2,}/gi, "options":[] , "type": "error"},
-    {"pattern":/¡/gi, "options":[] , "type": "error"},
-    {"pattern":/[0-9]S[0-9]/g, "options":[] , "type": "error"},
-]
+export const ruleSet =
+    [{'pattern': /\s;\s?/g, 'options': ['; '], 'type': 'error'},
+      {'pattern': /\s:\s?/g, 'options': [': '], 'type': 'error'},
+      {'pattern': /¾/g, 'options': ['1/3'], 'type': 'error', 'usageNote': 'wrong formatting of persentage ¾ sould be proofread as 3/4 '},
+      {'pattern': /¼/g, 'options': ['1/4'], 'type': 'error', 'usageNote': 'wrong formatting of persentage ¼ sould be proofread as 1/4 '},
+      {'pattern': /½/g, 'options': ['1/2'], 'type': 'error'},
+      {'pattern': /^\s/g, 'options': [''], 'type': 'error', 'usageNote': 'Space at begining of line'},
+      {'pattern': /(?:0-9]){,4}o/, 'options': ['°'], 'type': 'warning'},
+      {'pattern': /(?:[a-z]|\s)\-$/g, 'options': ['-', '--'], 'usageNote': 'bring other half of word on to this line', 'type': 'error'},
+      {'pattern': /\s{2,}/g, 'options': [' '], 'usageNote': 'double space', 'type': 'warning'},
+      {'pattern': /\s-{1,4}\s/g, 'options': ['--'], 'usageNote': 'start a line with an em-dash. move the em-dash and first word of the sentance to previous line'},
+      {'pattern': /\s?\(\s/g, 'options': [' ('], 'type': 'warning'},
+      {'pattern': /\s\)\s?/g, 'options': [' )'], 'type': 'warning'},
+      {'pattern': /\.\s\.\s\./g, 'options': ['...', '. ...'], 'type': 'error'},
+      {'pattern': /\.\.\.\s(\.|\?|\!)/g, 'options': ['...'], 'type': 'error'},
+      {'pattern': /\d{1,}(nd|rd|th|st)/g, 'options': [], 'usageNote': 'the part of place should be superscript eg.2<sup>nd</sup><br><br> eg 2^{nd}', 'type': 'error'},
+      {'pattern': /(\s|^)["|'](\s|$)/g, 'options': [], 'type': 'error'},
+      {'pattern': /[1-9]\s[1-9]{1,}\/[1-9]{1,}/g, 'options': [], 'type': 'error'},
+      {'pattern': /\s[\!|\?]/g, 'options': [], 'type': 'error'},
+      {'pattern': /~/g, 'options': [], 'type': 'error'},
+      {'pattern': /\b[=|\*]\b/gi, 'options': [], 'type': 'error'},
+      {'pattern': /æ/g, 'options': ['[ae]'], 'type': 'warning'},
+      {'pattern': /œ/g, 'options': ['[oe]'], 'type': 'warning'},
+      {'pattern': /\_/g, 'options': ['-'], 'type': 'special'},
+      {'pattern': /¯/g, 'options': [], 'type': 'error'},
+      {'pattern': /¨/g, 'options': [], 'type': 'error'},
+      {'pattern': /·/g, 'options': [], 'type': 'error'},
+      {'pattern': /`/g, 'options': [], 'type': 'error'},
+      {'pattern': /´/g, 'options': [], 'type': 'error'},
+      {'pattern': /∨/g, 'options': [], 'type': 'error'},
+      {'pattern': /∪/g, 'options': [], 'type': 'error'},
+      {'pattern': /˜/g, 'options': [], 'type': 'error'},
+      {'pattern': /¸/g, 'options': [], 'type': 'error'},
+      {'pattern': /\^/g, 'options': [], 'type': 'special'},
+      {'pattern': /(?:^|\w|\s)(\*)(?:\w|\s|$)/g, 'options': ['[*]'], 'type': 'warning'},
+      {'pattern': /ſ/g, 'options': ['s', 'S'], 'type': 'error'},
+      {'pattern': /[α-ωΑ-Ω]/g, 'options': [], 'type': 'error', 'usageNote': 'greek lettering see guide on wiki'},
+      {'pattern': /\—/g, 'options': ['-', '--'], 'type': 'error'},
+      {'pattern': /[†|‡|§]/g, 'options': ['[*]'], 'type': 'error', 'usageNote': 'please replace alterntive with aphophy *'},
+      {'pattern': /(?:[a-z])[A-Z]{1,}/g, 'options': [], 'type': 'error'},
+      {'pattern': /[\“|\”]/g, 'options': ['"'], 'type': 'error', 'usageNote': 'please replace with "'},
+      {'pattern': /(?:\w)\"(?:\w)/g, 'options': [], 'type': 'error'},
+      {'pattern': /\"\"/g, 'options': ['"'], 'type': 'error'},
+      {'pattern': /[´|`]/g, 'options': ['\''], 'type': 'error'},
+      {'pattern': /\w\s\-{1,}\s\w/g, 'options': [], 'type': 'error'},
+      {'pattern': /\w\s\-{1,}/g, 'options': [], 'type': 'error'},
+      {'pattern': /-{3}/g, 'options': ['----', '--', '-'], 'type': 'error'},
+      {'pattern': /\s\!/g, 'options': ['!'], 'type': 'error'},
+      {'pattern': /^-[\w]/g, 'options': [], 'type': 'error'},
+      {'pattern': /\w*\s('nt|'s|'re|n't|'is)/g, 'options': [], 'type': 'error'},
+      {'pattern': /-{1,}/g, 'options': [], 'type': 'warning'},
+      {'pattern': /\w\(/g, 'options': [], 'type': 'warning'},
+      {'pattern': /\)\w/g, 'options': [], 'type': 'warning'},
+      {'pattern': /^[0-9]{1,}$/g, 'options': [], 'type': 'warning', 'usageNote': 'please make sure you\'ve removed all page numbers'},
+      {'pattern': /(?:[\w|;])\.{3}(?!\.)}/g, 'options': [], 'type': 'error'},
+      {'pattern': /\.{3,4}(?:\w)/g, 'options': [], 'type': 'error'},
+      {'pattern': /[\!|\.|\?]\s\.{3}/g, 'options': [], 'type': 'error'},
+      {'pattern': /\"\'/g, 'options': [], 'type': 'warning'},
+      {'pattern': /\/'/g, 'options': [], 'type': 'warning'},
+      {'pattern': /''/g, 'options': [], 'type': 'warning'},
+      {'pattern': /\b1\b/g, 'options': [], 'type': 'warning'},
+      {'pattern': /(?:\s|^)([b-hj-z]\b)/gi, 'options': [], 'type': 'warning'},
+      {'pattern': /^(\}|\)|\])/gi, 'options': [], 'type': 'error'},
+      {'pattern': /^(\,|\.|\?|\!)/gi, 'options': [], 'type': 'error'},
+      {'pattern': /(?:\w)(\.)(?:\w)/g, 'options': [], 'type': 'error'},
+      // {"pattern": /[;|\.|\,|:]{2,}/g, "options":[] , "type": "error"},
+      {'pattern': /¡/g, 'options': [], 'type': 'error'},
+      {'pattern': /[0-9]S[0-9]/g, 'options': [], 'type': 'warning'},
+      {'pattern': /\b\/(\w|$)/g, 'options': [], 'type': 'warning'},
+      {'pattern': /(\s)(\"|\')$/g, 'options': [], 'type': 'error'},
+      {'pattern': /^(;|:)/g, 'options': [], 'type': 'error'},
+      {'pattern': /\w,\w/g, 'options': [], 'type': 'error'},
+    // {"pattern": /Figure\:? [0-9]/gi, "options":[] , "type": "warning"},
+    ];
 // rules to add
 // rules to add space before end of line mark
 // 1+1 to 1 + 1  same with = done
 // * to [*] warning done
 // æ œ done
 // _ between word instead of - Done
-///[α-ωΑ-Ω]*/ Done
-//—
+// /[α-ωΑ-Ω]*/ Done
+// —
 // ... correct Done
 // ¯ ¨ · ` ´ ˆ ∨ ∪ ˜¸Done
-// H_{2}O, 0_{2}, CO_{2} 
+// H_{2}O, 0_{2}, CO_{2}
 // remove -- eol warning
 //  º
 // *, †, ‡, §, replace with * Done
 // [U+0400–U+04FF] celyrlic
 // [Hebrew: **], or [Arabic: **].
 // ( ) { }
-// warnings 
+// warnings
 // ſ long s done
 // `
 // “ ”. done
 // would n't 't is did n't what 's  they 're does n't /done
 // ---
-//?...
-//{ "pattern": /\*/g, "options":["[*]"] , "type": "warning" },
-//{ "pattern": /\^/g, "options":[] , "type": "special"},
-//{ "pattern": /H20/gi, "options":["H_{2}O"] , "type": "warning"},
-//{ "pattern": /C2O/gi, "options":["C_{2}O"] , "type": "warning"},
-//{ "pattern": /O2/gi, "options":["O_{2}"] , "type": "warning"},
+// ?...
+// {"pattern": /\*/g, "options":["[*]"] , "type": "warning" },
+// {"pattern": /\^/g, "options":[] , "type": "special"},
+// {"pattern": /H20/gi, "options":["H_{2}O"] , "type": "warning"},
+// {"pattern": /C2O/gi, "options":["C_{2}O"] , "type": "warning"},
+// {"pattern": /O2/gi, "options":["O_{2}"] , "type": "warning"},
 // \w"\w
 // 0'
-//Captitals in middle of word partaily done
-//[ \w
+// Captitals in middle of word partaily done
+// [ \w
 
 
-// a)prean 
-//They -they make the sky look done
-// ' 
-//-'
+// a)prean
+// They -they make the sky look done
+// '
+// -'
 // ¡
+// .-
+// .[*]
+// --?
